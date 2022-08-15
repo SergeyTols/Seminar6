88b7219db2d76828aff34d5f4ea7be0631b43c50
Sarge <sarge2033@gmail.com>
2022-08-15 22:07:45 +0300

    x3

---
+++ Sem4/Program.cs
@@ -0,0 +1,22 @@
+﻿static int[] CreateArray(int[] num)
+{
+    int[] array = new int[num];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = i + 1;
+    }
+    return array;
+}
+static void WriteArray(int[] array)
+{
+        foreach(int num in array)
+        
+    {
+        Console.Write(num + " ");
+    }
+    static void Main(string[] args)
+    {
+        WriteArray(Createarray(20));
+    }
+    
+}
